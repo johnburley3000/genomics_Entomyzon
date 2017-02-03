77052721861635c411f44e6da168dd2cf39ae921
johnburley3000 <john.burley3000@gmail.com>
2017-02-02 20:51:05 -0500

    adds a line to draw a plot
    mutation rate is kept as for human example

--- run_psmcprep2.sh
+++ run_psmcprep2.sh
@@ -28,3 +28,5 @@ module load gnuplot/4.6.4-fasrc01
 /n/home01/jtb/apps/psmc/utils/fq2psmcfa -q20 ${SAMPLE}_dip_ts.fq > ${SAMPLE}.psmcfa
 psmc -N25 -t15 -r5 -p "4+25*2+4+6" -o ${SAMPLE}.psmc ${SAMPLE}.psmcfa
 
+/n/home01/jtb/apps/psmc/utils/psmc_plot.pl -u 3.83e-08 -g 1 -p ${SAMPLE}_plot ${SAMPLE}.psmc
+
