766ae44f543faad633810310fb8cf0676286a4d6
johnburley3000 <john.burley3000@gmail.com>
2017-02-20 17:55:24 -0500

    type q20 to q30, line 28

--- run_psmcprep2.sh
+++ run_psmcprep2.sh
@@ -25,7 +25,7 @@ vcfutils.pl vcf2fq -d 10 -D 50 -Q 30 > ${SAMPLE}_dip_ts.fq
 module load psmc/0.6.5-fasrc01
 module load gnuplot/4.6.4-fasrc01
 
-/n/home01/jtb/apps/psmc/utils/fq2psmcfa -q20 ${SAMPLE}_dip_ts.fq > ${SAMPLE}.psmcfa
+/n/home01/jtb/apps/psmc/utils/fq2psmcfa -q30 ${SAMPLE}_dip_ts.fq > ${SAMPLE}.psmcfa
 psmc -N25 -t15 -r5 -p "4+25*2+4+6" -o ${SAMPLE}.psmc ${SAMPLE}.psmcfa
 
 /n/home01/jtb/apps/psmc/utils/psmc_plot.pl -u 3.83e-08 -g 1 -p ${SAMPLE}_plot ${SAMPLE}.psmc
